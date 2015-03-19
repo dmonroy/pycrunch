7f9a5ffa76adc13387839b2846ba84a3107f6ea0
Jason R. Coombs <jaraco@jaraco.com>
2015-03-19 05:02:39 -0400

    Fix text failures by treating the csv module differently on Python 2 than Python 3, and encoding the output after rendering on Python 3. Fixes #5.

--- pycrunch/csvlib.py
+++ pycrunch/csvlib.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+import io
 import csv
 import six
 
@@ -13,28 +14,43 @@ def rows_as_csv_file(rows, none_as_no_data=True):
     the "No Data" system missing value. If False, None values are emitted
     normally by the Python csv library, which means as (quoted) empty strings.
     """
-    # Write to a BytesIO because it joins encoded lines as we go
+    # Write to a StringIO because it joins lines as we go
     # and a .read() of it (like requests.post will do) does not make a copy.
-    out = six.BytesIO()
+    out = io.StringIO() if six.PY3 else io.BytesIO()
 
     if none_as_no_data:
         sentinel = "__CSV_SENTINEL_NONE__"
 
         class EphemeralWriter():
             def write(self, line):
-                line = line.replace('"' + sentinel + '"', "")
-                out.write(line.encode('utf-8'))
+                line = line.replace(str('"' + sentinel + '"'), str(""))
+                out.write(line)
         pipe = EphemeralWriter()
 
         writer = csv.writer(pipe, quoting=csv.QUOTE_NONNUMERIC, lineterminator='\n')
         for row in rows:
             row = [sentinel if cell is None else cell for cell in row]
+            row = [
+                cell.encode('utf-8')
+                    if six.PY2 and isinstance(cell, six.text_type)
+                    else cell
+                for cell in row
+            ]
             writer.writerow(row)
     else:
         writer = csv.writer(out, quoting=csv.QUOTE_NONNUMERIC, lineterminator='\n')
         for row in rows:
+            row = [
+                cell.encode('utf-8')
+                    if six.PY2 and isinstance(cell, six.text_type)
+                    else cell
+                for cell in row
+            ]
             writer.writerow(row)
 
     out.seek(0)
+
+    if six.PY3:
+        out = io.BytesIO(out.getvalue().encode('utf-8'))
 
     return out
