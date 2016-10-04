c8d2cbd8304774064f1f61be1935d805968de33d
Alejandro Rivera <alejandro.rivera@msn.com>
2016-10-04 03:04:30 -0500

    Initial implementation of exclusion expressions, using python's built-in AST parser module. More to come.

--- pycrunch/datasets.py
+++ pycrunch/datasets.py
@@ -1,16 +1,148 @@
+import ast
 import json
+
+
+NOT_IN = object()
+
+
+def parse_expr(expr):
+
+    def _parse(node):
+        obj = {}
+        args = []
+        op = None
+
+        if isinstance(node, ast.AST):
+            # Get the current node fields.
+            fields = list(ast.iter_fields(node))
+
+            # "Terminal" nodes. Recursion ends with these guys.
+            if isinstance(node, ast.Name):
+                _id = fields[0][1]
+                return {
+                    'variable': _id
+                }
+            elif isinstance(node, ast.Num) or isinstance(node, ast.Str):
+                _val = fields[0][1]
+                return {
+                    'value': _val
+                }
+            elif isinstance(node, ast.Eq):
+                return '=='
+            elif isinstance(node, ast.NotEq):
+                return '!='
+            elif isinstance(node, ast.Lt):
+                return '<'
+            elif isinstance(node, ast.LtE):
+                return '<='
+            elif isinstance(node, ast.Gt):
+                return '>'
+            elif isinstance(node, ast.GtE):
+                return '>='
+            elif isinstance(node, ast.In):
+                return 'in'
+            elif isinstance(node, ast.NotIn):
+                return NOT_IN
+            elif isinstance(node, ast.List):
+                _list = fields[0][1]
+                if not (all(isinstance(el, ast.Str) for el in _list) or
+                        all(isinstance(el, ast.Num) for el in _list)):
+                    # Only list-of-int or list-of-str are currently supported
+                    raise ValueError
+
+                return {
+                    'value': [
+                        getattr(el, 's', None) or getattr(el, 'n')
+                        for el in _list
+                    ]
+                }
+
+            # "Non-terminal" nodes.
+            else:
+                for _name, _val in fields:
+                    if isinstance(_val, ast.BoolOp) or isinstance(_val, ast.Compare):
+                        # Descend.
+                        obj.update(_parse(_val))
+                    elif isinstance(_val, ast.And):
+                        op = 'and'
+                    elif isinstance(_val, ast.Or):
+                        op = 'or'
+                    elif _name == 'left':
+                        left = _parse(_val)
+                        args.append(left)
+                    elif _name == 'ops':
+                        if len(_val) != 1:
+                            raise ValueError
+                        op = _parse(_val[0])
+                    elif _name == 'comparators':  # i.e. "the right side"
+                        if len(_val) != 1:
+                            raise ValueError
+                        right = _parse(_val[0])
+                        args.append(right)
+                    elif isinstance(_val, list):
+                        args = [_parse(arg) for arg in _val]
+
+                if op:
+                    if op is NOT_IN:
+                        # Special treatment for the `not in` operator.
+                        obj = {
+                            'function': 'not',
+                            'args': [
+                                {
+                                    'function': 'in',
+                                    'args': []
+                                }
+                            ]
+                        }
+                    else:
+                        obj = {
+                            'function': op,
+                            'args': []
+                        }
+
+            if args and 'args' in obj:
+                if op is NOT_IN:
+                    # Special treatment for the `not in` operator.
+                    obj['args'][0]['args'] = args
+                else:
+                    obj['args'] = args
+
+        return obj
+
+    if expr is None:
+        return dict()
+
+    return _parse(ast.parse(expr, mode='eval'))
+
+
+def post_process_expr(obj, variables):
+    """
+    Transform variable aliases into variable IDs (i.e. URLs)
+    """
+    for key, val in obj.items():
+        if isinstance(val, dict):
+            post_process_expr(val, variables)
+        elif isinstance(val, list) or isinstance(val, tuple):
+            for subitem in val:
+                post_process_expr(subitem, variables)
+        elif key == 'variable':
+            obj[key] = variables[val].entity.self
+
+    return obj
 
 
 def exclusion(ds, expr=None):
     """
     Given a dataset object, apply an exclusion filter to it (defined as an
-    expression dictionary).
-    """
-    if expr is None:
-        # An empty dictionary will effectively clear the exclusion filter.
-        expr = dict()
+    expression string).
 
+    If the `expr` parameter is None, an empty expression object is sent
+    as part of the PATCH request, which effectively removes the exclusion
+    filter (if any).
+    """
+    expr_obj = parse_expr(expr)
+    expr_obj = post_process_expr(expr_obj, ds.variables.by('alias'))
     return ds.session.patch(
         ds.fragments.exclusion,
-        data=json.dumps(dict(expression=expr))
+        data=json.dumps(dict(expression=expr_obj))
     )
--- pycrunch/tests/test_datasets.py
+++ pycrunch/tests/test_datasets.py
@@ -4,6 +4,7 @@ from unittest import TestCase
 from unittest import mock
 
 from pycrunch.datasets import exclusion
+from pycrunch.datasets import parse_expr
 
 
 class TestExclusionFilters(TestCase):
@@ -15,22 +16,37 @@ class TestExclusionFilters(TestCase):
         Tests that the proper PATCH request is sent to Crunch in order to
         apply an exclusion filter to a dataset.
         """
-        exclusion_filter = {
-            'function': '!=',
-            'args': [
-                {'variable': 'disposition.id'},
-                {'value': 0}
-            ]
-        }
+        exclusion_filter = 'disposition != 0'
+        var_url = 'http://test.crunch.io/api/datasets/123/variables/0001/'
 
+        # Mocking setup.
         ds = mock.MagicMock()
         ds.fragments.exclusion = self.exclusion_url
+        _disposition_mock = mock.MagicMock()
+        _disposition_mock.entity.self = var_url
+        ds.variables.by.return_value = {
+            'disposition': _disposition_mock
+        }
 
+        # Action!
         exclusion(ds, exclusion_filter)
-        ds.session.patch.assert_called_once_with(
-            self.exclusion_url,
-            data=json.dumps({'expression': exclusion_filter})
-        )
+
+        # Ensure .patch was called the right way.
+        assert len(ds.session.patch.call_args_list) == 1
+
+        call = ds.session.patch.call_args_list[0]
+        assert call[0][0] == self.exclusion_url
+
+        expected_expr_obj = {
+            'expression': {
+                'function': '!=',
+                'args': [
+                    {'variable': var_url},  # Crunch needs variable URLs!
+                    {'value': 0}
+                ]
+            }
+        }
+        assert json.loads(call[1]['data']) == expected_expr_obj
 
     def test_remove_exclusion(self):
         """
@@ -46,3 +62,163 @@ class TestExclusionFilters(TestCase):
             self.exclusion_url,
             data=json.dumps({'expression': {}})
         )
+
+
+class TestFilterExpressionParsing(TestCase):
+
+    def test_parse_single_test_comparison(self):
+        expr = "name != 'John Doe'"
+        expr_obj = parse_expr(expr)
+        assert expr_obj == {
+            'function': '!=',
+            'args': [
+                {
+                    'variable': 'name'
+                },
+                {
+                    'value': 'John Doe'
+                }
+            ]
+        }
+
+        # Reversed.
+        expr = "'John Doe' != name"
+        expr_obj = parse_expr(expr)
+        assert expr_obj == {
+            'function': '!=',
+            'args': [
+                {
+                    'value': 'John Doe'
+                },
+                {
+                    'variable': 'name'
+                }
+            ]
+        }
+
+    def test_parse_single_numeric_comparison(self):
+        expr = "caseid <= 1234"
+        expr_obj = parse_expr(expr)
+        assert expr_obj == {
+            'function': '<=',
+            'args': [
+                {
+                    'variable': 'caseid'
+                },
+                {
+                    'value': 1234
+                }
+            ]
+        }
+
+        # Reversed.
+        expr = "1234 >= caseid"
+        expr_obj = parse_expr(expr)
+        assert expr_obj == {
+            'function': '>=',
+            'args': [
+                {
+                    'value': 1234
+                },
+                {
+                    'variable': 'caseid'
+                }
+            ]
+        }
+
+    def test_parse_compare_variable_against_another_variable(self):
+        expr = "starttdate == arrivedate"
+        expr_obj = parse_expr(expr)
+        assert expr_obj == {
+            'function': '==',
+            'args': [
+                {
+                    'variable': 'starttdate'
+                },
+                {
+                    'variable': 'arrivedate'
+                }
+            ]
+        }
+
+    def test_parse_multiple_boolean_conditions(self):
+        expr = '(identity == 1 and caseid <= surveyid) or identity >= 2'
+        expr_obj = parse_expr(expr)
+        assert expr_obj == {
+            'function': 'or',
+            'args': [
+                {
+                    'function': 'and',
+                    'args': [
+                        {
+                            'function': '==',
+                            'args': [
+                                {
+                                    'variable': 'identity'
+                                },
+                                {
+                                    'value': 1
+                                }
+                            ]
+                        },
+                        {
+                            'function': '<=',
+                            'args': [
+                                {
+                                    'variable': 'caseid'
+                                },
+                                {
+                                    'variable': 'surveyid'
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'function': '>=',
+                    'args': [
+                        {
+                            'variable': 'identity'
+                        },
+                        {
+                            'value': 2
+                        }
+                    ]
+                }
+            ]
+        }
+
+    def test_parse_value_in_list(self):
+        expr = "web_browser in ['abc', 'dfg', 'hij']"
+        expr_obj = parse_expr(expr)
+        assert expr_obj == {
+            'function': 'in',
+            'args': [
+                {
+                    'variable': 'web_browser'
+                },
+                {
+                    'value': ['abc', 'dfg', 'hij']
+                }
+            ]
+        }
+
+    def test_parse_value_not_in_list(self):
+        expr = 'country not in [1, 2, 3]'
+        expr_obj = parse_expr(expr)
+        assert expr_obj == {
+            'function': 'not',
+            'args': [
+                {
+                    'function': 'in',
+                    'args': [
+                        {
+                            'variable': 'country'
+                        },
+                        {
+                            'value': [1, 2, 3]
+                        }
+                    ]
+                }
+            ]
+        }
