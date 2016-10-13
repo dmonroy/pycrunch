629fb65b4a2ef070c76aa11a3cffd673b7ea5761
Alejandro Rivera <alejandro.rivera@msn.com>
2016-10-13 14:03:28 -0500

    Add tests for "recodes" and "transformations" to the "integration tests" script.

--- pycrunch/tests/integration/pycrunch_workflow_integration_test.py
+++ pycrunch/tests/integration/pycrunch_workflow_integration_test.py
@@ -4,6 +4,9 @@ import os
 
 import pycrunch
 from pycrunch import pandaslib
+from pycrunch.transformations import create_categorical
+from pycrunch.recodes import combine_categories
+from pycrunch.recodes import combine_responses
 
 
 CRUNCH_URL = os.environ.get('CRUNCH_TEST_URL')
@@ -411,6 +414,172 @@ def main():
         pycrunch.datasets.exclusion(dataset)
         df = pandaslib.dataframe(dataset)
         assert len(df) == len(ROWS) - 1  # excluding the header
+
+        # 2. Integration Tests for "Transformations".
+
+        categories = [
+            {'id': 1, 'name': 'Nerds', 'numeric_value': 1, 'missing': False},
+            {'id': 2, 'name': 'Normal Users', 'numeric_value': 2, 'missing': False},
+            {'id': 3, 'name': 'Hipsters', 'numeric_value': 3, 'missing': False},
+            {'id': 32767, 'name': 'Unknown', 'numeric_value': None, 'missing': True}
+        ]
+
+        rules = [
+            'operating_system in ("Linux", "Solaris", "Minix", "FreeBSD", "NetBSD")',
+            'operating_system == "Windows"',
+            'operating_system == "MacOS"',
+            'missing(operating_system)'
+        ]
+
+        new_var = create_categorical(
+            ds=dataset,
+            categories=categories,
+            rules=rules,
+            name='Operating System Users',
+            alias='operating_system_users',
+            description='Type of Operating System Users'
+        )
+        assert isinstance(new_var, pycrunch.shoji.Entity)
+        new_var.refresh()
+        assert new_var.body.type == 'categorical'
+
+        # Check the data on the new variable.
+        df = pandaslib.dataframe(dataset)
+        assert 'operating_system_users' in df
+
+        # Check the nerds.
+        assert len(df[df['operating_system_users'] == 'Nerds']) == 8
+        assert set(
+            r['operating_system']
+            for _, r in df[df['operating_system_users'] == 'Nerds'].iterrows()
+        ) == {'Linux', 'Solaris', 'Minix', 'FreeBSD', 'NetBSD'}
+
+        # Check the hipsters.
+        assert len(df[df['operating_system_users'] == 'Hipsters']) == 1
+        assert set(
+            r['operating_system']
+            for _, r in df[df['operating_system_users'] == 'Hipsters'].iterrows()
+        ) == {'MacOS'}
+
+        # Check normal users.
+        assert len(df[df['operating_system_users'] == 'Normal Users']) == 3
+        assert set(
+            r['operating_system']
+            for _, r in df[df['operating_system_users'] == 'Normal Users'].iterrows()
+        ) == {'Windows'}
+
+        # 3. Integration Tests for "Recodes".
+
+        # 3.1 combine_categories.
+
+        # On a 'categorical' variable.
+        cat_map = {
+            1: {
+                'name': 'Bilingual',
+                'missing': False,
+                'combined_ids': [2, 3]
+            },
+            2: {
+                'name': 'Not Bilingual',
+                'missing': False,
+                'combined_ids': [1, 4]
+            },
+            99: {
+                'name': 'Unknown',
+                'missing': True,
+                'combined_ids': [32766, 32767]
+            }
+        }
+        new_var = combine_categories(
+            dataset, 'speak_spanish', cat_map, 'Bilingual Person', 'bilingual'
+        )
+        assert isinstance(new_var, pycrunch.shoji.Entity)
+        new_var.refresh()
+        assert new_var.body.type == 'categorical'
+
+        df = pandaslib.dataframe(dataset)
+        assert 'bilingual' in df
+
+        # Check the data in the recoded variable.
+        assert len(df[df['bilingual'] == 'Bilingual']) == 5
+        assert set(
+            int(r['identity'])
+            for _, r in df[df['bilingual'] == 'Bilingual'].iterrows()
+        ) == {3, 4, 10, 11, 12}
+
+        assert len(df[df['bilingual'] == 'Not Bilingual']) == 4
+        assert set(
+            int(r['identity'])
+            for _, r in df[df['bilingual'] == 'Not Bilingual'].iterrows()
+        ) == {1, 2, 5, 6}
+
+        assert len(df[df['bilingual'].isnull()]) == 3
+        assert set(
+            int(r['identity'])
+            for _, r in df[df['bilingual'].isnull()].iterrows()
+        ) == {7, 8, 9}
+
+        # On a 'categorical_array' variable.
+        cat_map = {
+            1: {
+                'name': 'Interested',
+                'missing': False,
+                'combined_ids': [1, 2]
+            },
+            2: {
+                'name': 'Not interested',
+                'missing': False,
+                'combined_ids': [3, 4]
+            },
+            99: {
+                'name': 'Unknown',
+                'missing': True,
+                'combined_ids': [32766, 32767]
+            }
+        }
+        new_var = combine_categories(
+            dataset, 'hobbies', cat_map, 'Hobbies (recoded)', 'hobbies_recoded'
+        )
+        assert isinstance(new_var, pycrunch.shoji.Entity)
+        new_var.refresh()
+        assert new_var.body.type == 'categorical_array'
+
+        df = pandaslib.dataframe(dataset)
+        assert 'hobbies_recoded' in df
+
+        # Check the data in the recoded variable.
+        for _, row in df[['hobbies', 'hobbies_recoded']].iterrows():
+            hobbies = row['hobbies']
+            hobbies_rec = row['hobbies_recoded']
+            assert len(hobbies) == len(hobbies_rec)
+
+            for i, value in enumerate(hobbies):
+                if value in ({'?': 32766}, {'?': 32767}):
+                    assert hobbies_rec[i] == {'?': 99}
+                elif value in (1, 2):
+                    assert hobbies_rec[i] == 1
+                elif value in (3, 4):
+                    assert hobbies_rec[i] == 2
+
+        # 3.2 combine_responses.
+
+        response_map = {
+            'music_recoded_1': ['music_1', 'music_2'],
+            'music_recoded_2': ['music_97'],
+            'music_recoded_3': ['music_98', 'music_99']
+        }
+        new_var = combine_responses(
+            dataset, 'music', response_map, 'Music (alt)', 'music_recoded'
+        )
+        assert isinstance(new_var, pycrunch.shoji.Entity)
+        new_var.refresh()
+        assert new_var.body.type == 'multiple_response'
+
+        df = pandaslib.dataframe(dataset)
+        assert 'music_recoded' in df
+
+        # TODO: Test the data in the recoded variable. Unsure of its meaning.
+
     finally:
         dataset.delete()
 
