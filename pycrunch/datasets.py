71e6264c0ec2771ebd3886c1cfbfb284231e2165
Sebastian Kriems <bastoberto@gmx.de>
2016-10-15 12:11:44 +0200

    moved exclusion method to the Dataset class to enable `ds.exclusion(<expression>)` syntax

--- pycrunch/datasets.py
+++ pycrunch/datasets.py
@@ -12,27 +12,23 @@ class Dataset(Entity):
     class for adding methods to the dataset entity
     """
 
-    def my_method(self):
-        return "I'm a dataset method"
+    def exclusion(self, expr=None):
+        """
+        Given a dataset object, apply an exclusion filter to it (defined as an
+        expression string).
 
-
-def exclusion(ds, expr=None):
-    """
-    Given a dataset object, apply an exclusion filter to it (defined as an
-    expression string).
-
-    If the `expr` parameter is None, an empty expression object is sent
-    as part of the PATCH request, which effectively removes the exclusion
-    filter (if any).
-    """
-    if isinstance(expr, six.string_types):
-        expr_obj = parse_expr(expr)
-        expr_obj = process_expr(expr_obj, ds)  # cause we need variable URLs
-    elif expr is None:
-        expr_obj = {}
-    else:
-        expr_obj = expr
-    return ds.session.patch(
-        ds.fragments.exclusion,
-        data=json.dumps(dict(expression=expr_obj))
-    )
+        If the `expr` parameter is None, an empty expression object is sent
+        as part of the PATCH request, which effectively removes the exclusion
+        filter (if any).
+        """
+        if isinstance(expr, six.string_types):
+            expr_obj = parse_expr(expr)
+            expr_obj = process_expr(expr_obj, self)  # cause we need variable URLs
+        elif expr is None:
+            expr_obj = {}
+        else:
+            expr_obj = expr
+        return self.session.patch(
+            self.fragments.exclusion,
+            data=json.dumps(dict(expression=expr_obj))
+        )
