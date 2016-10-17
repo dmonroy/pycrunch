69203b05b2a25d59e0c77cd5dbba7314a389e419
Sebastian Kriems <bastoberto@gmx.de>
2016-10-17 09:32:19 +0200

    create_categorical is now a Dataset class method

--- pycrunch/datasets.py
+++ pycrunch/datasets.py
@@ -32,3 +32,40 @@ class Dataset(Entity):
             self.fragments.exclusion,
             data=json.dumps(dict(expression=expr_obj))
         )
+
+    def create_categorical(self, categories, rules,
+                           name, alias, description=''):
+
+        """
+        creates a categorical variable deriving from other variables
+        """
+
+        if not ((len(categories) - 1) <= len(rules) <= len(categories)):
+            raise ValueError(
+                'Amount of rules should match categories (or categories -1')
+
+        if not hasattr(self, 'variables'):
+            self.refresh()
+
+        args = [{
+            'column': [c['id'] for c in categories],
+            'type': {
+                'value': {
+                    'class': 'categorical',
+                    'categories': categories}}}]
+
+        more_args = []
+        for rule in rules:
+            more_args.append(parse_expr(rule))
+
+        more_args = process_expr(more_args, self)
+
+        expr = dict(function='case', args=args + more_args)
+
+        payload = dict(element='shoji:entity',
+                       body=dict(alias=alias,
+                                 name=name,
+                                 expr=expr,
+                                 description=description))
+
+        return self.variables.create(payload)
